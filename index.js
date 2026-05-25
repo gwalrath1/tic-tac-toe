2d3a606782164d0f160bf735c6127e002abab56a
gabriella walrath <gabriella.walrath@gmail.com>
2026-05-25 17:33:00 -0400

    additional changes to css?

--- index.js
+++ index.js
@@ -29,47 +29,98 @@ $(document).ready(function () {
     let $box7 = $('#box7');
     let $box8 = $('#box8');
 
-    // Represents if click is x or o. But mine are supposed to be images so how would I do that...
-    let $player1 = $('X');
-    let $player2 = $('O');
+    let $turn = 1; // at the start of a game, no turns have happened
+    let $winner = false; // at the start of a game, we do not have a winner
 
-    let currentPlayer = 'X';
+    // hides alerts before start button is clicked
+    // might have to make it so i use a helper function to create the alerts bc rn they keep flashing RIGHT as the page loads. no bueno.
+     $('#alertStart').hide()
+     $('#alertWinner').hide()
+     $('#alertDraw').hide()
+    
+     const $winningCombos = [
+        [$box0, $box1, $box2], // rows
+        [$box3, $box4, $box5], 
+        [$box6, $box7, $box8],
+        [$box0, $box3, $box6], // columns
+        [$box1, $box4, $box7],
+        [$box2, $box5, $box8],
+        [$box0, $box4, $box8], // diagonals
+        [$box2, $box4, $box6]
+     ];
 
-    $('.box').on('click', function (e) {
-        if ($(this).children('img').length > 0) return; // already has image, skip  // if you click into the div we have (the div is represented by "this", look it up on jquery), if the div has a child element that is an image, don't allow user to click that particular spot again. 
 
-        // pulsates player text when it is that player's turn.
-        let i = 0;
-        function pulsate() {
-            if (i >= 3) return;
-            $("#playerOne").
-                animate({ opacity: '0.5' }, 1000, 'linear').
-                animate({
-                    opacity: '1',
-                    width: "200px"
-                }, 1000, 'linear', pulsate)
+    // invoke custom function in query by writing $.customFunction(). Alternatively, $(this).customFunction if you wanna call it on a specific thing. So like $.checkWinner()
+    $.fn.checkWinner = function(boxA, boxB, boxC) {
+        
+        // left off here at 58:02 in video
+        if(boxA.$img.html() === 'images/x_resized.png' && boxB.$img.html() === 'images/x_resized.png' && boxB.$img.html() === 'images/x_resized.png') { // don't ask me if this is correct, just jotting my thoughts down lol.
+            $winner = true;
+            console.log("X is the winner!");
+            boxA.css({"background-color": "#FA2FBD", "opacity": "0.5"});
+            boxB.css({"background-color": "#FA2FBD", "opacity": "0.5"});
+            boxC.css({"background-color": "#FA2FBD", "opacity": "0.5"});
+        } else {
+            $winner = true;
+            console.log("O is the winner!");
+            boxA.css({"background-color": "#FA2FBD", "opacity": "0.5"});
+            boxB.css({"background-color": "#FA2FBD", "opacity": "0.5"});
+            boxC.css({"background-color": "#FA2FBD", "opacity": "0.5"});
         }
-
-        if (currentPlayer === 'X') {
-            pulsate();
-        }
-        let imgSrc = currentPlayer === 'X' ? 'images/x_resized.png' : 'images/o_resized.png';
-        // Create the img and append it into the clicked div
-        let $img = $('<img>').attr('src', imgSrc);
-        $(this).append($img);
-        // Switch turns
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    });
-
-    $("#playerOne").animate({
-
-    })
-
-
-    function generateXOrO() {
-        return currentPlayer;
-
-
+    }
+    
+    let $currentPlayer = 'X';
+     // function that starts game
+    $.fn.startGame = function() { // i'm actually tweaking this WORKED AHAHAHAHA
+        console.log("start game");
+        $("#alertStart").show();
     }
 
+    // CHILL I ACTUALLY WROTE JQUERY ON MY OWN WOW I PULLED MYSELF UP BY THE BOOSTRAPS (YAY)
+
+    let $startBtn = $('#startBtn');
+    console.log($startBtn);
+
+    // actually starts game
+    $startBtn.click(function() {
+        $(this).startGame();
+    });
+
+    // clicking each box to make it show x or o images
+    $('.box').on('click', function (e) {
+        $("#alertStart").hide(); // hides start alert
+
+        // check if there is a winner after min 5 turns
+        if($turn > 4) {
+            console.log("Is there a winner yet?");
+        }
+
+        if ($(this).children('img').length > 0) return; // already has image, skip  // if you click into the div we have (the div is represented by "this", look it up on jquery), if the div has a child element that is an image, don't allow user to click that particular spot again. this is the html element you're focused on
+        
+        // visually indicating which player needs to take a turn
+        
+       
+        console.log("Turn: " + $turn++);
+        let $imgSrc = $currentPlayer === 'X' ? 'images/x_resized.png' : 'images/o_resized.png';
+        // Create the img and append it into the clicked div
+        let $img = $('<img>').attr('src', $imgSrc);
+        $(this).append($img);
+        // Switch turns
+        $currentPlayer = $currentPlayer === 'X' ? 'O' : 'X';
+
+        console.log("Current player: " + $currentPlayer);
+        if($currentPlayer === 'X') {
+            $("#p1").addClass("border border-info rounded") // goes kind of fast, may have to tweak
+            $("#p2").removeClass("border border-info rounded");  
+        } else {
+            $("#p2").addClass("border border-info rounded");
+            $("#p1").removeClass("border border-info rounded"); 
+        }
+    });
+
+    // reset button that reloads page
+    let $resetBtn = $('#resetBtn');
+    $resetBtn.click(function() {
+        location.reload(true);
+    });
 });
