567c2d79b5a91a0c5a4ffbafd786d87b9cbd87fd
gabriella walrath <gabriella.walrath@gmail.com>
2026-07-08 20:25:07 -0400

    new and most current JS draft

---
+++ index_draft_4.js
@@ -0,0 +1,119 @@
+// // hides alerts before start button is clicked
+// // might have to make it so i use a helper function to create the alerts bc rn they keep flashing RIGHT as the page loads. no bueno.
+// $('#alertStart').hide()
+// $('#alertWinner').hide()
+// $('#alertDraw').hide()
+
+let turn = 1;
+let currentPlayer = 'X';
+let plays = {
+    X: [],
+    O: []
+};
+
+const winningCombinations = [
+    [0, 1, 2], // 0 // rows
+    [3, 4, 5], // 1
+    [6, 7, 8], // 2
+    [0, 3, 6], // 3  // columns
+    [1, 4, 7], // 4
+    [2, 5, 8], // 5
+    [0, 4, 8], // 6 // diagonals
+    [2, 4, 6]  // 7
+];
+
+$(document).ready(function () {
+
+
+
+    const $startBtn = $("#startBtn");
+    const $resetBtn = $('#resetBtn');
+    $resetBtn.click(function () {
+        location.reload(true);
+    });
+
+    $(".box").css("pointer-events", "none");
+
+    $startBtn.click(function () {
+        console.log("Start game");
+        $(".box").css("pointer-events", "auto");
+        $(".box").on('click', function () {
+            if (currentPlayer === 'X') {
+                console.log("Turn " + turn);
+                if ($(this).children('img').length > 0) {
+                    console.log("image already here");
+                    return;
+                } else {
+                    $(this).append('<img src="images/x-neon.png">');
+                    plays.X.push(parseInt($(this).attr("data-index")));
+                    console.log("Boxes for X: " + plays.X);
+                    console.log("Boxes for O: " + plays.O);
+                    if (isThereAWinner()) {
+                        console.log("We have a winner! it is " + currentPlayer)
+                        alert('Winner is: ' + currentPlayer);
+                        $(".box").css("pointer-events", "none");
+                        $(".box").off();
+
+                    }
+
+                    currentPlayer = 'O';
+                    turn++;
+                }
+            } else {
+                console.log("Turn " + turn);
+                if ($(this).children('img').length > 0) {
+                    console.log("image already here");
+                    return;
+                } else {
+                    $(this).append('<img src="images/o-neon.png">');
+                    plays.O.push(parseInt($(this).attr("data-index")));
+                    console.log("Boxes for X: " + plays.X);
+                    console.log("Boxes for O: " + plays.O);
+                    if (isThereAWinner()) {
+                        console.log("We have a winner! it is " + currentPlayer)
+                        alert('Winner is: ' + currentPlayer);
+                        $(".box").css("pointer-events", "none");
+                        $(".box").off();
+  
+                    } 
+                    currentPlayer = 'X';
+                    turn++;
+                }
+            }
+
+            if (turn > 9) {
+                console.log("draw");
+                alert("draw. no winner.");
+                $(".box").css("pointer-events", "none");
+                $(".box").off();
+            }
+        });
+    });
+});
+
+function isThereAWinner() {
+    if (turn < 5) {
+        return;
+    }
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+        let winner = true;
+
+        for (let j = 0; j < winningCombinations[i].length; j++) {
+            if ($.inArray(winningCombinations[i][j], plays[currentPlayer]) < 0) {
+                console.log("No winner!");
+                winner = false;
+                break;
+            }
+        }
+
+        if (winner) {
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+
